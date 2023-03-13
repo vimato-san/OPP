cf9c08c59bbe94c9bc1670d80d97991643f6af5f
Vimato <monst3k003@gmail.com>
2023-03-13 21:38:17 +0700

    Add second option of task

--- lab2/var2.c
+++ lab2/var2.c
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "openmp-use-default-none"
 #include <stdio.h>
 #include <malloc.h>
 #include <math.h>
@@ -17,9 +19,7 @@ void vector_print(double *vector, size_t vector_size) {
 
 double vector_norm(const double *vector, size_t vector_size) {
     double sum = 0;
-    #pragma omp parallel for
     for (int i = 0; i < vector_size; i++) {
-        #pragma omp atomic
         sum += (vector[i] * vector[i]);
     }
     return sqrt(sum);
@@ -27,27 +27,27 @@ double vector_norm(const double *vector, size_t vector_size) {
 
 
 void vector_sub(const double *vec_1, const double *vec_2, double *dest, size_t size) {
-    #pragma omp parallel for
+    #pragma omp for
     for (int i = 0; i < size; i++)
         dest[i] = vec_1[i] - vec_2[i];
 }
 
 
 void vector_mult_scalar(const double *vector, double scalar, double *dest, size_t size) {
-    #pragma omp parallel for
+    #pragma omp for
     for (int i = 0; i < size; i++)
         dest[i] = vector[i] * scalar;
 }
 
 
 void vector_mult_matrix(const double *vector, const double *matrix, double *result, size_t N) {
-    #pragma omp parallel for
-        for (int i = 0; i < N; i++) {
-                double sum = 0;
-                for (int j = 0; j < N; j++)
-                    sum += matrix[i * N + j] * vector[j];
-                result[i] = sum;
-        }
+    #pragma omp for
+    for (int i = 0; i < N; i++) {
+        double sum = 0;
+        for (int j = 0; j < N; j++)
+            sum += matrix[i * N + j] * vector[j];
+        result[i] = sum;
+    }
 }
 
 
@@ -64,16 +64,6 @@ void init_matrices(double *A, double *b, double *x, size_t N) {
 }
 
 
-double vector_accuracy(double *Ax, double *b, double b_norm, size_t N) {
-    double *tmp = (double *)malloc(sizeof(double) * N);
-    vector_sub(Ax, b, tmp, N);
-    double res = vector_norm(tmp, N) / b_norm;
-
-    free(tmp);
-    return res;
-}
-
-
 int main(int argc, char *argv[]) {
     size_t N = SIZE;
     double *A = (double *)malloc(sizeof(double) * N * N);
@@ -81,20 +71,33 @@ int main(int argc, char *argv[]) {
     double *Ax = (double *)malloc(sizeof(double) * N);
     double *b = (double *)malloc(sizeof(double) * N);
 
+    double *Ax_minus_b = (double *)malloc(sizeof(double) * N);  //this buffer is contained (Ax-b) result
+
     init_matrices(A, b, x, N);
     double time_begin = omp_get_wtime();
-    double b_norm = vector_norm(b, N);
-    vector_mult_matrix(x, A, Ax, N);
+    double b_norm;
 
     double accuracy = 1;
-
-    while (accuracy > EPSILON) {
-        vector_sub(Ax, b, Ax, N);
-        vector_mult_scalar(Ax, TAU, Ax, N);
-        vector_sub(x, Ax, x, N);
+    #pragma omp parallel
+    {
         vector_mult_matrix(x, A, Ax, N);
-        accuracy = vector_accuracy(Ax, b, b_norm, N);
-        printf("%.10f\n", accuracy);
+        #pragma omp single
+        {
+            b_norm = vector_norm(b, N);
+        }
+        while (accuracy > EPSILON) {
+            vector_sub(Ax, b, Ax, N);
+            vector_mult_scalar(Ax, TAU, Ax, N);
+            vector_sub(x, Ax, x, N);
+            vector_mult_matrix(x, A, Ax, N);
+
+            vector_sub(Ax, b, Ax_minus_b, N);
+            #pragma omp single
+            {
+                accuracy = vector_norm(Ax_minus_b, N) / b_norm;
+                printf("%.10f\n", accuracy);
+            }
+        }
     }
     vector_print(x, N);
     double time_end = omp_get_wtime();
@@ -104,5 +107,8 @@ int main(int argc, char *argv[]) {
     free(x);
     free(b);
     free(A);
+    free(Ax_minus_b);
     return 0;
 }
+
+#pragma clang diagnostic pop
